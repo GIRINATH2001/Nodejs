f44fcfc076a38726ceef8d5b444ff6dfd0366b43
E.L.GIRINATH <119178591+GIRINATH2001@users.noreply.github.com>
2024-03-25 18:10:00 +0530

    Create load_all_items_by_outletid.js

---
+++ load_all_items_by_outletid.js
@@ -0,0 +1,20 @@
+load_all_items_by_outletid 1
+
+
+'use strict';
+
+module.exports = (req, res) => {
+	var url = req.url;
+
+	switch (url) {
+		case '/':
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write('<h1>Hello from index.js<h1>');
+			break;
+		default:
+			res.writeHead(404);
+			res.write('You might find the page you are looking for at "/" path');
+			break;
+	}
+	res.end();
+};
